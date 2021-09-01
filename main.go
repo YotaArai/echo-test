b0ae3f35dab3045ab9e7fe024c824a43de0cd34c
YotaArai <yotaarai85@gmail.com>
2021-09-02 01:02:14 +0900

    feat: GORMでデータベースとの接続を実装

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-	"net/http"
-	"github.com/labstack/echo/v4"
+	"echo-test/database"
+
+  "net/http"
+
+  "github.com/labstack/echo/v4"
 )
 
+
+func hello(c echo.Context) error {
+  database.Connect()
+  sqlDB, _ := database.DB.DB()
+  defer sqlDB.Close()
+  err := sqlDB.Ping()
+  if err != nil {
+    return c.String(http.StatusInternalServerError, "データベース接続失敗")
+  } else {
+    return c.String(http.StatusOK, "Hello, World!")
+  }
+}
+
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":3000"))
+  e := echo.New()
+  e.GET("/", hello)
+  e.Logger.Fatal(e.Start(":3000"))
 }
