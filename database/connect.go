d41388f2d82ad0ac085db1b107b3fabd93ae0f63
YotaArai <yotaarai85@gmail.com>
2021-09-06 23:52:05 +0900

    fix: 環境変数によるDB接続先切替方法を修正

--- database/connect.go
+++ database/connect.go
@@ -2,7 +2,6 @@ package database
 
 import (
   "os"
-  "net/url"
 
   "gorm.io/driver/postgres"
   "gorm.io/gorm"
@@ -12,21 +11,15 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-  dsn := ""
-  if(os.Getenv("APP_ENV") == "local"){
-    user := os.Getenv("DB_USERNAME")
-    password := os.Getenv("DB_PASSWORD")
-    host := os.Getenv("DB_HOST")
-    port := os.Getenv("DB_PORT")
-    database := os.Getenv("DB_DATABASE")
 
-    dsn = "host=" + host + " user=" + user + " password=" + password + " dbname=" + database + " port=" + port + " sslmode=disable TimeZone=Asia/Tokyo"
-    
-  }else{
-    mydb_dsn := os.Getenv("DATABASE_URL")
-    parsed_dsn, _ := url.Parse(mydb_dsn)
-    dsn = parsed_dsn.String()
-    dsn += " sslmode=require"
-  }
+  user := os.Getenv("DB_USERNAME")
+  password := os.Getenv("DB_PASSWORD")
+  host := os.Getenv("DB_HOST")
+  port := os.Getenv("DB_PORT")
+  database := os.Getenv("DB_DATABASE")
+  sslmode := os.Getenv("DB_SSLMODE")
+
+  dsn := "host=" + host + " user=" + user + " password=" + password + " dbname=" + database + " port=" + port + " sslmode=" + sslmode + " TimeZone=Asia/Tokyo"
+  
   DB, _ = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 }
