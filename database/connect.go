aa25b214c32a4e23e2d91a515ae3ec58efb26ef4
YotaArai <yotaarai85@gmail.com>
2021-09-06 18:35:16 +0900

    heroku環境のDB接続方法を変更

--- database/connect.go
+++ database/connect.go
@@ -2,6 +2,7 @@ package database
 
 import (
   "os"
+  "net/url"
 
   "gorm.io/driver/postgres"
   "gorm.io/gorm"
@@ -11,13 +12,21 @@ import (
 var DB *gorm.DB
 
 func Connect() {
+  dsn := ""
+  if(os.Getenv("APP_ENV") == "local"){
+    user := os.Getenv("DB_USERNAME")
+    password := os.Getenv("DB_PASSWORD")
+    host := os.Getenv("DB_HOST")
+    port := os.Getenv("DB_PORT")
+    database := os.Getenv("DB_DATABASE")
 
-  user := os.Getenv("DB_USERNAME")
-  password := os.Getenv("DB_PASSWORD")
-  host := os.Getenv("DB_HOST")
-  port := os.Getenv("DB_PORT")
-  database := os.Getenv("DB_DATABASE")
-
-	dsn := "host=" + host + " user=" + user + " password=" + password + " dbname=" + database + " port=" + port + " sslmode=disable TimeZone=Asia/Tokyo"
+    dsn = "host=" + host + " user=" + user + " password=" + password + " dbname=" + database + " port=" + port + " sslmode=disable TimeZone=Asia/Tokyo"
+    
+  }else{
+    mydb_dsn := os.Getenv("DATABASE_URL")
+    parsed_dsn, _ := url.Parse(mydb_dsn)
+    dsn = parsed_dsn.String()
+    dsn += " sslmode=require"
+  }
   DB, _ = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 }
